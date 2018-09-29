3f88e2c3a74f7b9041a50983dd0174413c9d318e
Caleb Gammon <calebwgammon@gmail.com>
2018-09-29 13:57:49 -0400

    first commit

--- server.js
+++ server.js
@@ -11,48 +11,52 @@ const app = express();
 // a particular user.
 //
 // ALSO, for this challenge, we're storing user passwords as
-// plain text. This is something you should NEVER EVER EVER 
+// plain text. This is something you should NEVER EVER EVER
 // do in a real app. Instead, always use cryptographic
 // password hashing best practices (aka, the tried and true
 // ways to keep user passwords as secure as possible).
 // You can learn more about password hashing later
 // here: https://crackstation.net/hashing-security.htm
 const USERS = [
-  {id: 1,
-   firstName: 'Joe',
-   lastName: 'Schmoe',
-   userName: 'joeschmoe@business.com',
-   position: 'Sr. Engineer',
-   isAdmin: true,
-   // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
-   password: 'password'
+    {
+        id: 1,
+        firstName: 'Joe',
+        lastName: 'Schmoe',
+        userName: 'joeschmoe@business.com',
+        position: 'Sr. Engineer',
+        isAdmin: true,
+        // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
+        password: 'password'
   },
-  {id: 2,
-   firstName: 'Sally',
-   lastName: 'Student',
-   userName: 'sallystudent@business.com',
-   position: 'Jr. Engineer',
-   isAdmin: true,
-   // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
-   password: 'password'
+    {
+        id: 2,
+        firstName: 'Sally',
+        lastName: 'Student',
+        userName: 'sallystudent@business.com',
+        position: 'Jr. Engineer',
+        isAdmin: true,
+        // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
+        password: 'password'
   },
-  {id: 3,
-   firstName: 'Lila',
-   lastName: 'LeMonde',
-   userName: 'lila@business.com',
-   position: 'Growth Hacker',
-   isAdmin: false,
-   // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
-   password: 'password'
+    {
+        id: 3,
+        firstName: 'Lila',
+        lastName: 'LeMonde',
+        userName: 'lila@business.com',
+        position: 'Growth Hacker',
+        isAdmin: false,
+        // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
+        password: 'password'
   },
-  {id: 4,
-   firstName: 'Freddy',
-   lastName: 'Fun',
-   userName: 'freddy@business.com',
-   position: 'Community Manager',
-   isAdmin: false,
-   // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
-   password: 'password'
+    {
+        id: 4,
+        firstName: 'Freddy',
+        lastName: 'Fun',
+        userName: 'freddy@business.com',
+        position: 'Community Manager',
+        isAdmin: false,
+        // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
+        password: 'password'
   }
 ];
 
@@ -64,27 +68,48 @@ const USERS = [
 //  4. if matching user found, add the user object to the request object
 //     (aka, `req.user = matchedUser`)
 function gateKeeper(req, res, next) {
-  // your code should replace the line below
-  next();
+    const {
+        user,
+        pass
+    } = Object.assign({
+        user: null,
+        pass: null
+    }, queryString.parse(req.get('x-username-and-password')));
+    req.user = USERS.find((usr, index) => usr.userName === user && usr.password === pass);
+    next();
 }
 
 // Add the middleware to your app!
 
 // this endpoint returns a json object representing the user making the request,
-// IF they supply valid user credentials. This endpoint assumes that `gateKeeper` 
+// IF they supply valid user credentials. This endpoint assumes that `gateKeeper`
 // adds the user object to the request if valid credentials were supplied.
 app.get("/api/users/me", (req, res) => {
-  // send an error message if no or wrong credentials sent
-  if (req.user === undefined) {
-    return res.status(403).json({message: 'Must supply valid user credentials'});
-  }
-  // we're only returning a subset of the properties
-  // from the user object. Notably, we're *not*
-  // sending `password` or `isAdmin`.
-  const {firstName, lastName, id, userName, position} = req.user;
-  return res.json({firstName, lastName, id, userName, position});
+    // send an error message if no or wrong credentials sent
+    if (req.user === undefined) {
+        return res.status(403).json({
+            message: 'Must supply valid user credentials'
+        });
+    }
+    // we're only returning a subset of the properties
+    // from the user object. Notably, we're *not*
+    // sending `password` or `isAdmin`.
+    const {
+        firstName,
+        lastName,
+        id,
+        userName,
+        position
+    } = req.user;
+    return res.json({
+        firstName,
+        lastName,
+        id,
+        userName,
+        position
+    });
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Your app is listening on port ${process.env.PORT}`);
+    console.log(`Your app is listening on port ${process.env.PORT}`);
 });
